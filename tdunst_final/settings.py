874652402ef1983231ffb826720ceab25beae1e8
Tyler <tcdunster@quinnipiac.edu>
2015-11-19 19:28:44 +0000

    Setup Heroku

--- tdunst_final/settings.py
+++ tdunst_final/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'registration',
+    'django.contrib.sites',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -116,3 +118,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(MAIN_DIR, 'static'),)
 STATIC_ROOT = 'staticfiles'
+LOGIN_URL = '/user/login'
+LOGIN_REDIRECT_URL = '/'
+SITE_ID = 1
