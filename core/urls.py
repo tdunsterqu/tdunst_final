906ba91ea235d0d6e3584f1fa995e46197741484
Tyler <tcdunster@quinnipiac.edu>
2015-12-06 19:18:41 +0000

    Add required user authentication

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 from .views import *
 
 urlpatterns = patterns('',
    url(r'^$', Home.as_view(), name='home'),
    url(r'^user/', include('registration.backends.simple.urls')),
    url(r'^user/', include('django.contrib.auth.urls')),
-   url(r'^review/create/$', ReviewCreateView.as_view(), name='review_create'),
-   url(r'review/$', ReviewListView.as_view(), name='review_list'),
-   url(r'^review/(?P<pk>\d+)/$', ReviewDetailView.as_view(), name='review_detail'),
-   url(r'^review/update/(?P<pk>\d+)/$', ReviewUpdateView.as_view(), name='review_update'),
-   url(r'^review/delete/(?P<pk>\d+)/$', ReviewDeleteView.as_view(), name='review_delete'),
-   url(r'^review/(?P<pk>\d+)/comment/create/$', CommentCreateView.as_view(), name='comment_create'),
-   url(r'^review/(?P<review_pk>\d+)/comment/update/(?P<comment_pk>\d+)/$', CommentUpdateView.as_view(), name='comment_update'),
-   url(r'^review/(?P<review_pk>\d+)/comment/delete/(?P<comment_pk>\d+)/$', CommentDeleteView.as_view(), name='comment_delete'),
+   url(r'^review/create/$', login_required(ReviewCreateView.as_view()), name='review_create'),
+   url(r'review/$', login_required(ReviewListView.as_view()), name='review_list'),
+   url(r'^review/(?P<pk>\d+)/$', login_required(ReviewDetailView.as_view()), name='review_detail'),
+   url(r'^review/update/(?P<pk>\d+)/$', login_required(ReviewUpdateView.as_view()), name='review_update'),
+   url(r'^review/delete/(?P<pk>\d+)/$', login_required(ReviewDeleteView.as_view()), name='review_delete'),
+   url(r'^review/(?P<pk>\d+)/comment/create/$', login_required(CommentCreateView.as_view()), name='comment_create'),
+   url(r'^review/(?P<review_pk>\d+)/comment/update/(?P<comment_pk>\d+)/$', login_required(CommentUpdateView.as_view()), name='comment_update'),
+   url(r'^review/(?P<review_pk>\d+)/comment/delete/(?P<comment_pk>\d+)/$', login_required(CommentDeleteView.as_view()), name='comment_delete'),
 )
