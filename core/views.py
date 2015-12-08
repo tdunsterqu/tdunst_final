eafb9cd50811c91420f883946061127646bfefae
Tyler <tcdunster@quinnipiac.edu>
2015-12-08 17:40:49 +0000

    Add Reviews and Comments to User Profiles

--- core/views.py
+++ core/views.py
@@ -127,6 +127,15 @@ class UserDetailView(DetailView):
   template_name= 'user/user_detail.html'
   context_object_name = 'user_in_view'
 
+  def get_context_data(self, **kwargs):
+    context = super(UserDetailView, self).get_context_data(**kwargs)
+    user_in_view= User.objects.get(username=self.kwargs['slug'])
+    reviews = Review.objects.filter(user=user_in_view)
+    context['reviews'] = reviews
+    comments = Comment.objects.filter(user=user_in_view)
+    context['comments'] = comments
+    return context
+
 
 
 
